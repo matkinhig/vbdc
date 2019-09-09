0e158b5d1652060bb51bce7a0d4bbfe3c5c946e1
matkinhig <matkinhig@gmail.com>
2019-09-09 13:29:06 +0700

    commit layout loginid

---
+++ 1_iOSVietBank/AccountTableList/DataAccountViewController.h
@@ -0,0 +1,17 @@
+//
+//  DataAccountViewController.h
+//  1_iOSVietBank
+//
+//  Created by Lực Nguyễn  on 9/9/19.
+//  Copyright © 2019 Lực Nguyễn . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DataAccountViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
